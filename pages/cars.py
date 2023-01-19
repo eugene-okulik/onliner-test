f549defe7975df596295131907c35ce4c90307c9
eugeny.okulik <tiburon854@gmail.com>
2023-01-19 21:16:51 +0300

    First tests and tests structure created

---
+++ pages/cars.py
@@ -0,0 +1,40 @@
+from pages.base_page import BasePage
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+sort_dropdown = (
+    By.CLASS_NAME,
+    'vehicle-form__link vehicle-form__link_other vehicle-form__link_base vehicle-form__link_arrow_bottom'
+)
+first_car_name = (
+    By.CLASS_NAME,
+    'vehicle-form__link vehicle-form__link_primary-alter vehicle-form__link_middle vehicle-form__link_noreflex'
+)
+options_list = (
+    By.CLASS_NAME,
+    (
+        'dropdown-style dropdown-style_primary dropdown-style_small dropdown-style_bottom-right '
+        'vehicle-form__dropdown vehicle-form__dropdown_width_xxs dropdown-style_visible'
+    )
+)
+
+
+class CarsPage(BasePage):
+    def __init__(self, driver):
+        super().__init__(driver)
+
+    def open(self):
+        self.driver.get('https://ab.onliner.by/')
+
+    def click_sort_dropdown(self):
+        self.find(sort_dropdown).click()
+
+    def first_car_name_text(self):
+        return self.find(first_car_name).text
+
+    def select_option(self, option_id):
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(options_list))
+        self.find(options_list).find_elements(By.CLASS_NAME, 'dropdown-style__item')[option_id].click()
+
---
+++ tests/test_cars_page.py
@@ -0,0 +1,13 @@
+from pages.cars import CarsPage
+import pytest
+
+
+@pytest.mark.parametrize('option_id', list(range(1, 8)))
+def test_click_dropdown(driver, option_id):
+    cars_page = CarsPage(driver)
+    cars_page.open()
+    text_before_sort = cars_page.first_car_name_text()
+    cars_page.click_sort_dropdown()
+    cars_page.select_option(option_id)
+    text_after_sort = cars_page.first_car_name_text()
+    assert text_before_sort != text_after_sort
