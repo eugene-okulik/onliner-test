78596258eba58e6c7911bafd12488bbb14029bec
Eugeny Okulik <tiburon854@gmail.com>
2023-07-04 21:34:15 +0300

    fix attempt

--- conftest.py
+++ conftest.py
@@ -7,7 +7,7 @@ import pytest
 def driver():
     options = Options()
     options.add_argument('--headless')
-    chrome_driver = webdriver.Chrome()
+    chrome_driver = webdriver.Chrome(options=options)
     chrome_driver.implicitly_wait(2)
     chrome_driver.maximize_window()
     yield chrome_driver
